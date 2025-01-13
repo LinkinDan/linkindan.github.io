5f0455221a2a8ad5a5a93af39b160fa73038aac1
LinkinDan <76703246+LinkinDan@users.noreply.github.com>
2025-01-13 16:41:51 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function playAudio(audioId) {
-    var audio = document.getElementById(audioId);
-    audio.play()
-}
-
-function toggleMenu() {
-    const dropdown = document.getElementById('mobileMenu');
-    dropdown.classList.toggle('open');
-}
